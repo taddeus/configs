cb70a0d921c794a29b51360f948f69ad21359e38
Taddeus Kroes <taddeuskroes@gmail.com>
2014-12-01 14:04:26 +0100

    Cleanup

--- bootstrap.bash
+++ bootstrap.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s -T $DIR/bashrc ~/.bashrc
-ln -s -T $DIR/vimrc ~/.vimrc
-ln -s -T $DIR/vim ~/.vim
+ln -s $DIR/bashrc ~/.bashrc
+ln -s $DIR/vimrc ~/.vimrc
+ln -sT $DIR/vim ~/.vim
