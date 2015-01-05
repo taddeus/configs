124a754b21dbf51b66fae43935974ca123e9e47f
Taddeus Kroes <taddeuskroes@hotmail.com>
2015-01-05 13:24:44 +0100

    Updated bootstrap script

--- bootstrap.bash
+++ bootstrap.bash
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+git submodule update --init
+
 ln -s $DIR/bashrc ~/.bashrc
 ln -s $DIR/vimrc_plugins ~/.vimrc
 ln -sT $DIR/vim ~/.vim
 
 source ~/.bashrc
 
-vim -c PluginInstall
-cd $DIR/vim/bundle/YouCompleteMe && ./install.sh --clang-completer
+vim +PluginInstall +qall
+cd $DIR/vim/bundle/YouCompleteMe && ./install.sh
