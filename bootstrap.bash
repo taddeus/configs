38ffc42a438d34b225cd24ddc836a3099b34216e
Taddeus Kroes <taddeuskroes@gmail.com>
2015-08-04 16:37:30 +0200

    Bootstrap script dependency fix

--- bootstrap.bash
+++ bootstrap.bash
@@ -3,12 +3,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 git submodule update --init
 
-ln -s $DIR/bashrc ~/.bashrc
-ln -s $DIR/vimrc_plugins ~/.vimrc
-ln -sT $DIR/vim ~/.vim
-ln -s $DIR/gitignore ~/.gitignore
+ln -sf $DIR/bashrc ~/.bashrc
+ln -sf $DIR/vimrc_plugins ~/.vimrc
+ln -sfT $DIR/vim ~/.vim
+ln -sf $DIR/gitconfig ~/.gitconfig
 
 source ~/.bashrc
 
+sudo apt-get install -y build-essential cmake python-dev
 vim +PluginInstall +qall
 cd $DIR/vim/bundle/YouCompleteMe && ./install.sh
