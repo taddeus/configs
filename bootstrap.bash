f2b7b64956db5c0cd4c927e5f29b5de286c9cce5
Taddeus Kroes <taddeuskroes@gmail.com>
2017-01-23 15:04:03 +0100

    Update bootstrap script

--- bootstrap.bash
+++ bootstrap.bash
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-sudo apt-get install -y git vim-nox
+if ! which git >/dev/null; then
+    echo "Error: git is not installed"
+    exit 1
+fi
 
+# git
 git submodule update --init
-
-ln -sf $DIR/bashrc ~/.bashrc
-ln -sf $DIR/vimrc ~/.vimrc
-ln -sfT $DIR/vim ~/.vim
 ln -sf $DIR/gitconfig ~/.gitconfig
 
+# bash
+ln -sf $DIR/bashrc ~/.bashrc
 source ~/.bashrc
 
-vim +PluginInstall +qall
+# vim
+if which vim >/dev/null; then
+    ln -sfT $DIR/vim ~/.vim
+    ln -sf $DIR/vimrc ~/.vimrc
+    vim +PluginInstall +qall
+fi
